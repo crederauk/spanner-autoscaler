e3bc444cd73d01ccf066538a017385d5ae0ced38
Nick Griffiths <nicholas.griffiths@dmwgroup.co.uk>
2020-12-20 16:35:00 +0000

    Missing copyright & license header

--- src/main/kotlin/com/dmwgroup/gcp/spanner/autoscaler/Configuration.kt
+++ src/main/kotlin/com/dmwgroup/gcp/spanner/autoscaler/Configuration.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2020 Andrew James <andrew.james@dmwgroup.co.uk> and DMW Group Ltd.
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
 package com.dmwgroup.gcp.spanner.autoscaler
 
 import org.springframework.boot.context.properties.ConfigurationProperties
